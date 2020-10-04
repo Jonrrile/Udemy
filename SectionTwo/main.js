0ba2a842de3cecebf5faf5d9c0ac99771fef8b3b
Jon Riley <jonrrile@ut.utm.edu>
2020-10-04 16:02:29 -0500

    if else statements

--- SectionTwo/main.js
+++ SectionTwo/main.js
@@ -14,9 +14,9 @@ console.log(fullAge);
 var job;
 job = "macdaddy";
 console.log(job);
- 
 
-/// Variable mutation 
+
+/// Variable mutation
 var firstName = "Jonathan";
 var age = 26;
 
@@ -37,7 +37,7 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 var lastName = prompt('What is his last name?');
 console.log(lastName);
 
-Basic Operators 
+Basic Operators
 var year = 2020;
 var yearJonathan = 26;
 var yearMark = 33;
@@ -51,9 +51,8 @@ var legalAge = 18;
 
 var isLegalAge = now - yearJonathan >= legalAge;
 console.log(isLegalAge);
-*/
 
-// Coding challenge one 
+// Coding challenge one
 
 var johnHeight = 60;
 var markHeight = 72;
@@ -67,3 +66,14 @@ var markBmi = markMass / (markHeight * markHeight);
 var higherBmi = markBmi > johnBmi;
 console.log(higherBmi);
 console.log("Is mark's BMI higher than John's?", higherBmi);
+*/
+
+// if else statements 
+var firstName = "Jonathan";
+var civilStatus = "single";
+
+if (civilStatus == "married") {
+    console.log(firstName + " is married");
+} else {
+    console.log(firstName + " will hopefully marry soon.");
+}
